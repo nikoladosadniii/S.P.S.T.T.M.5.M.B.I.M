ed5654cc1464c2cca45d87bb7114aeffb1a87980
Nikola <nikoladosadni@outlook.com>
2026-07-10 20:07:34 +0200

    Updated a typo in idk.py

--- idk.py
+++ idk.py
@@ -1,4 +1,4 @@
 a=int(input("Enter a number"))
 b=int(input("Enter a second number"))
 print("a + b is{a+b} and a-b is {a-b}")
-print("if a and b were days,it would be{a+b}days,{(a+b)*24}hours,{(a+b)*24*60}minutes or {(a+b)*24*60*60}seconds."
+print("if a and b were days,it would be{a+b}days,{(a+b)*24}hours,{(a+b)*24*60}minutes or {(a+b)*24*60*60}seconds.")
